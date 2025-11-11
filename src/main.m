7b9498e2a59fc45eb0a50d42e96151d346c1541b
topelj <jaxontopel2021@gmail.com>
2025-11-11 16:53:45 +0000

    Main sim changes

--- src/main.m
+++ src/main.m
@@ -2,3 +2,33 @@
 % ECE 6390 - Satellite Communication & Navigation Systems
 % Georgia Institute of Technology
 % =======================================================
+
+% ==========
+% Parameters
+% ==========
+B = 1e6;        % Bandwidth in Hz
+Fs = 10 * B;    % Sampling frequency
+N = 100;        % Number of samples
+
+% Generate and upsample the pilot sequence for Modulation
+pilot = generatePilot(B, Fs, N);
+
+% Applies modulation using the carrier frequency
+tx_signal = modulatePilot(pilot, fc, Fs);
+
+% Computes received power using Friss equation??
+rx_signal = applyPathLoss(tx_signal, pt, Gt, Gr, fc, R);
+
+% RF Link analysis abstracted function ??
+
+% Adds white gaussian noise and jamming to simulate interference
+rx = addNoiseAndJamming(rx_signal, pilot, upsample_factor);
+
+% Performs cross-correlation to extract phase offset from pilot reference
+phase_offset = estimatePhase(rx_noisy, pilot, upsample_factor);
+
+% Combines received signals coherently or incoherently depending on mode.
+beam = combineBeams(rx_signals, phase_offsets, mode);
+
+% Generates plots for visualizing pilot and phase alignment.
+plotResults(t, pilot, rx_signal, rx_noisy, phase_offsets)
