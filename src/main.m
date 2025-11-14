ac261c2539698e1cb1d2596886210d33a63835ad
jaxon <jaxontopel2021@gmail.com>
2025-11-14 13:37:21 +0000

    Cleanup

--- src/main.m
+++ src/main.m
@@ -7,14 +7,14 @@ clc; clear; close all;
 % ==========
 % Parameters
 % ==========
-params.B = 1e6;        % Bandwidth in Hz
-params.Fs = 10 * params.B;    % Sampling frequency
-params.N = 100;        % Number of samples
-params.fc = 2e9;        % Carrier frequency in Hz
-params.pt = 1;          % Transmit power in Watts
-params.Gt = 10;         % Gain of the transmitting antenna in dBi
-params.Gr = 10;         % Gain of the receiving antenna in dBi
-params.R = 1000;        % Distance in meters
+params.B = 1e6;             % Bandwidth in Hz
+params.Fs = 10 * params.B;  % Sampling frequency
+params.N = 100;             % Number of samples
+params.fc = 2e9;            % Carrier frequency in Hz
+params.pt = 1;              % Transmit power in Watts
+params.Gt = 10;             % Gain of the transmitting antenna in dBi
+params.Gr = 10;             % Gain of the receiving antenna in dBi
+params.R = 1000;            % Distance in meters
 params.upsample_factor = 4; % Upsampling factor for the pilot signal
 
 % =======================================================
@@ -28,15 +28,15 @@ tx_signal = modulatePilot(pilot, params);
 % =======================================================
 
 % Computes received power using Friss equation??
-rx_signal = applyPathLoss(tx_signal, pt, Gt, Gr, fc, R);
+rx_signal = applyPathLoss(tx_signal, params);
 
 % RF Link analysis abstracted function ??
 
 % Adds white gaussian noise and jamming to simulate interference
-rx = addNoiseAndJamming(rx_signal, pilot, upsample_factor);
+rx = addNoiseAndJamming(rx_signal, pilot, params);
 
 % Performs cross-correlation to extract phase offset from pilot reference
-phase_offset = estimatePhase(rx_noisy, pilot, upsample_factor);
+phase_offset = estimatePhase(rx, pilot, params);
 
 % Combines received signals coherently or incoherently depending on mode.
 beam = combineBeams(rx_signals, phase_offsets, mode);
